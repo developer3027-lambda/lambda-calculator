418f8a94de3a49b8af9eb5c1faa48e0d50f3bcbc
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-08 16:09:58 -0400

    Problem with Numbers.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+
 const NumberButton = () => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <div>
+    <button 
+    className='' 
+    onClick={() => props.number()}
+    ></button>
+    {/* Display a button element rendering the data being passed down from the parent container on props */}
+    </div>
   );
-};
+}
+
+export default NumberButton
